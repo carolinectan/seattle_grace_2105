6443091f19327e1961d8412dfe4d4d796b501bdd
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 12:47:43 -0400

    Add hospital test; need to refactor doctors_by_hospital method

---
+++ lib/network.rb
@@ -0,0 +1,27 @@
+class Network
+  attr_reader :name,
+              :hospitals
+
+  def initialize(name)
+    @name = name
+    @hospitals = []
+  end
+
+  def add_hospital(hospital)
+    @hospitals << hospital
+  end
+
+  def highest_paid_doctor
+    (@hospitals.flat_map { |hospital| hospital.doctors}).max_by { |doctor| doctor.salary}
+  end
+
+  def doctors_by_hospital
+    doctors_by_hospital = Hash.new { |key, value| hash[key] = [] }
+
+    @hospitals.select do |hospital|
+      hospital.doctors.select do |hospital, doctor|
+        doctors_by_hospital[hospital] << doctor.name
+      end
+    end
+  end
+end
