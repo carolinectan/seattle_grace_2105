259c2a6bd40dec26bf8bf19b15c99f117e55f847
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 16:42:39 -0400

    Refactor testsfor organization

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -2,50 +2,46 @@ require 'rspec'
 require './lib/doctor'
 require './lib/hospital'
 
-RSpec.describe do Doctor
-  before :each do
-    @meredith = Doctor.new({
-      name: "Meredith Grey",
-      specialty: "General Surgery",
-      education: "Harvard University",
-      salary: 100_000})
+RSpec.describe Hospital do
+  describe 'instantiation' do
+    before :each do
+      @meredith = Doctor.new({
+        name: "Meredith Grey",
+        specialty: "General Surgery",
+        education: "Harvard University",
+        salary: 100_000})
 
-    @alex = Doctor.new({
-      name: "Alex Karev",
-      specialty: "Pediatric Surgery",
-      education: "Johns Hopkins University",
-      salary: 90_000})
+      @alex = Doctor.new({
+        name: "Alex Karev",
+        specialty: "Pediatric Surgery",
+        education: "Johns Hopkins University",
+        salary: 90_000})
 
-    @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
-  end
+      @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
+    end
 
-  it "exists" do
-    expect(@meredith).to be_an_instance_of(Doctor)
-    expect(@alex).to be_an_instance_of(Doctor)
-    expect(@seattle_grace).to be_an_instance_of(Hospital)
-  end
+    it "exists" do
+      expect(@meredith).to be_an_instance_of(Doctor)
+      expect(@alex).to be_an_instance_of(Doctor)
+      expect(@seattle_grace).to be_an_instance_of(Hospital)
+    end
 
-  it "has a hospital name" do
-    expect(@seattle_grace.name).to eq("Seattle Grace")
-  end
+    it "has attributes on instantiation" do
+      expect(@seattle_grace.name).to eq("Seattle Grace")
+      expect(@seattle_grace.chief_of_surgery).to eq("Richard Webber")
+      expect(@seattle_grace.doctors).to eq([@meredith, @alex])
+    end
 
-  it "has a Chief of Surgury" do
-    expect(@seattle_grace.chief_of_surgery).to eq("Richard Webber")
-  end
+    it "has a total salary" do
+      expect(@seattle_grace.total_salary).to eq(190000)
+    end
 
-  it "has doctors" do
-    expect(@seattle_grace.doctors).to eq([@meredith, @alex])
-  end
+    it "has a lowest paid doctor" do
+      expect(@seattle_grace.lowest_paid_doctor).to eq("Alex Karev")
+    end
 
-  it "has a total salary" do
-    expect(@seattle_grace.total_salary).to eq(190000)
-  end
-
-  it "has a lowest paid doctor" do
-    expect(@seattle_grace.lowest_paid_doctor).to eq("Alex Karev")
-  end
-
-  it "has specialities for doctors" do
-    expect(@seattle_grace.specialties).to eq(["General Surgery", "Pediatric Surgery"])
+    it "has specialities for doctors" do
+      expect(@seattle_grace.specialties).to eq(["General Surgery", "Pediatric Surgery"])
+    end
   end
 end
