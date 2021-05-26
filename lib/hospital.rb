e117f5b719438d7125ca0b969c497f8c620648bb
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 12:46:52 -0400

    Add hospital methods

---
+++ lib/hospital.rb
@@ -0,0 +1,24 @@
+class Hospital
+  attr_reader :name,
+              :chief_of_surgery,
+              :doctors
+
+  def initialize(name, chief_of_surgery, doctors)
+    @name = name
+    @chief_of_surgery = chief_of_surgery
+    @doctors = doctors
+  end
+
+  def total_salary
+    (@doctors.flat_map { |doctor| doctor.salary }).sum
+  end
+
+  def lowest_paid_doctor
+    @doctors.min_by { |doctor| doctor.salary }.name
+  end
+
+  def specialties
+    @doctors.flat_map { |doctor| doctor.specialty }
+  end
+
+end
