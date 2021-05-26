0c05ccebcc90b0647234ae520d3c6e75d584cbf2
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 16:39:09 -0400

    Add describe instantiation do block

--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -1,22 +1,26 @@
 require 'rspec'
 require './lib/doctor'
 
-RSpec.describe do Doctor
+RSpec.describe Doctor do
+  describe 'instantiation' do
+    before :each do
+      @meredith = Doctor.new({
+        name: "Meredith Grey",
+        specialty: "General Surgery",
+        education: "Harvard University",
+        salary: 100_000
+      })
+    end
 
-  before :each do
-    @meredith = Doctor.new({
-      name: "Meredith Grey",
-      specialty: "General Surgery",
-      education: "Harvard University",
-      salary: 100_000})
-  end
+    it "exists" do
+      expect(@meredith).to be_an_instance_of(Doctor)
+    end
 
-  it "exists" do
-    expect(@meredith).to be_an_instance_of(Doctor)
-
-    expect(@meredith.name).to eq("Meredith Grey")
-    expect(@meredith.specialty).to eq("General Surgery")
-    expect(@meredith.education).to eq("Harvard University")
-    expect(@meredith.salary).to eq(100000)
+    it "has attributes" do
+      expect(@meredith.name).to eq("Meredith Grey")
+      expect(@meredith.specialty).to eq("General Surgery")
+      expect(@meredith.education).to eq("Harvard University")
+      expect(@meredith.salary).to eq(100000)
+    end
   end
 end
