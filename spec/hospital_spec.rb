6dba5168dfa1057cd6b30678a9575eba71ec4d0a
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 18:03:29 -0400

    Add describe blocks for more organization

--- spec/hospital_spec.rb
+++ spec/hospital_spec.rb
@@ -3,23 +3,23 @@ require './lib/doctor'
 require './lib/hospital'
 
 RSpec.describe Hospital do
+  before :each do
+    @meredith = Doctor.new({
+      name: "Meredith Grey",
+      specialty: "General Surgery",
+      education: "Harvard University",
+      salary: 100_000})
+
+    @alex = Doctor.new({
+      name: "Alex Karev",
+      specialty: "Pediatric Surgery",
+      education: "Johns Hopkins University",
+      salary: 90_000})
+
+    @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
+  end
+
   describe 'instantiation' do
-    before :each do
-      @meredith = Doctor.new({
-        name: "Meredith Grey",
-        specialty: "General Surgery",
-        education: "Harvard University",
-        salary: 100_000})
-
-      @alex = Doctor.new({
-        name: "Alex Karev",
-        specialty: "Pediatric Surgery",
-        education: "Johns Hopkins University",
-        salary: 90_000})
-
-      @seattle_grace = Hospital.new("Seattle Grace", "Richard Webber", [@meredith, @alex])
-    end
-
     it "exists" do
       expect(@meredith).to be_an_instance_of(Doctor)
       expect(@alex).to be_an_instance_of(Doctor)
@@ -31,16 +31,18 @@ RSpec.describe Hospital do
       expect(@seattle_grace.chief_of_surgery).to eq("Richard Webber")
       expect(@seattle_grace.doctors).to eq([@meredith, @alex])
     end
+  end
 
-    it "has a total salary" do
+  describe 'methods' do
+    it "can calculate a total salary" do
       expect(@seattle_grace.total_salary).to eq(190000)
     end
 
-    it "has a lowest paid doctor" do
+    it "returns name of lowest paid doctor" do
       expect(@seattle_grace.lowest_paid_doctor).to eq("Alex Karev")
     end
 
-    it "has specialities for doctors" do
+    it "returns specialities of doctors" do
       expect(@seattle_grace.specialties).to eq(["General Surgery", "Pediatric Surgery"])
     end
   end
