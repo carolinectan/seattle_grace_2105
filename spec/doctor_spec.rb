7c1c3349a78877d055e8689ade79724af6447658
Caroline Tan <carolinectan3@gmail.com>
2021-05-26 12:45:31 -0400

    Write doctor tests

---
+++ spec/doctor_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require './lib/doctor'
+
+
+RSpec.describe do Doctor
+
+  before :each do
+    @meredith = Doctor.new({
+      name: "Meredith Grey",
+      specialty: "General Surgery",
+      education: "Harvard University",
+      salary: 100_000})
+  end
+
+  it "exists" do
+    expect(@meredith).to be_an_instance_of(Doctor)
+
+    expect(@meredith.name).to eq("Meredith Grey")
+    expect(@meredith.specialty).to eq("General Surgery")
+    expect(@meredith.education).to eq("Harvard University")
+    expect(@meredith.salary).to eq(100000)
+  end
+
+
+
+
+
+end
